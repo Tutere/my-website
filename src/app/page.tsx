8e1bc2b28769c6a86ebddc5873087096b3cdb24f
tuteredurie <tuteredurie@hotmail.com>
2023-12-29 10:13:59 +1300

    shadcn UI setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
      <div>TEST</div>
+     <Button> TEST BUTTON </Button>
     </main>
   )
 }
