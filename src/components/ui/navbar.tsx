b12e244f009e3319524ec3f07ecefdcb1871e662
tuteredurie <tuteredurie@hotmail.com>
2024-01-07 09:01:37 +1300

    icon changed to name

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -10,7 +10,7 @@ export function Navbar () {
         <div className="flex items-center px-3 cursor-pointer">
           <Link href={"/"} >
             <h1 className="text-xl font-bold text-white cursor-pointer mr-8">
-              Icon placeholder
+              Tutere Durie
             </h1>
           </Link>
           <div className="flex items-center gap-2">
