3ae117b40df7801c7647bc7c025041c72c2349c7
tuteredurie <tuteredurie@hotmail.com>
2024-02-03 13:10:22 +1300

    tooltips added to project card buttons. From shadcn

--- src/components/ui/projectCard.tsx
+++ src/components/ui/projectCard.tsx
@@ -1,4 +1,11 @@
 import {Github,Link as LinkIcon} from 'lucide-react';
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip"
+  
 
 interface ProjectCardProps {
     img: string;
@@ -16,20 +23,37 @@ export default function ProjectCard({img, title, description,siteUrl,githubUrl}:
             style={{background: `url(${img})`, backgroundSize: "cover"}}
         >
             <div className="overlay flex items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
-                <a 
-                    href={siteUrl}
-                    target="_blank"
-                    className='h-14 w-14 mr-2 border-2 relative rounded-full border-gray-500 hover:border-white group/link'
-                >
-                    <LinkIcon className='h-10 w-10 text-gray-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:hover:text-white'></LinkIcon>
-                </a>
-                <a 
-                    href={githubUrl}
-                    target="_blank"
-                    className='h-14 w-14 border-2 relative rounded-full border-gray-500 hover:border-white group/link'
-                >
-                    <Github className='h-10 w-10 text-gray-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:hover:text-white'></Github>
-                </a>
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger className='h-14 w-14 mr-2 border-2 relative rounded-full border-gray-500 hover:border-white group/link'>
+                        <a 
+                            href={siteUrl}
+                            target="_blank"
+                            >
+                            <LinkIcon className='h-10 w-10 text-gray-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white'></LinkIcon>
+                        </a>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                        <p>View website</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger className='h-14 w-14 border-2 relative rounded-full border-gray-500 hover:border-white group/link'>
+                        <a 
+                        href={githubUrl}
+                        target="_blank"
+                        >
+                            <Github className='h-10 w-10 text-gray-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white'></Github>
+                        </a>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                        <p>View Github Repository</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
             </div>
         </div>
         <div className="text-white rounded-b-xl mt-2 px-4">
